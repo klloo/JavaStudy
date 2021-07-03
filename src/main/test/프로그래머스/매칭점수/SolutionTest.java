506c99f84fda2fcac7ec88da568727edcd0c2033
klloo <asdf016182@naver.com>
2021-07-03 15:02:25 +0900

    [프로그래머스] 매칭점수

--- src/main/test/프로그래머스/매칭점수/SolutionTest.java
+++ src/main/test/프로그래머스/매칭점수/SolutionTest.java
@@ -12,8 +12,8 @@ public class SolutionTest {
         Solution solution = new Solution();
         String word = "Muzi";
         String[] pages = {"<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&& \n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"};
-        int ans = solution.solution(word,pages);
-        System.out.println(ans);
+        int answer = solution.solution(word,pages);
+        System.out.println(answer);
     }
 
 }
