152478e322f1fb91176e89ed5a79acbb5cc7b4ea
klloo <53117014+klloo@users.noreply.github.com>
2021-12-31 22:36:56 +0900

    [백준] 1753번 : 최단경로

--- src/main/java/백준/최단경로/Solution.java
+++ src/main/java/백준/최단경로/Solution.java
@@ -23,17 +23,17 @@ public class Solution {
         int V = sc.nextInt();
         int E = sc.nextInt();
         int k = sc.nextInt();
-        Map<Integer,List<Pair>> graph = new HashMap<>();
+
+        List<Pair>[] graph = new ArrayList[V+1];
+        for(int i=1; i<=V; i++)
+            graph[i] = new ArrayList<>();
+
+        // 그래프 생성
         for(int i=0; i<E; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
             int w = sc.nextInt();
-            // 그래프 생성
-            List<Pair> list = new ArrayList<Pair>();
-            if(graph.containsKey(u))
-                list = graph.get(u);
-            list.add(new Pair(v,w));
-            graph.put(u, list);
+            graph[u].add(new Pair(v,w));
         }
         // dist 배열 무한대 값으로 초기화
         int[] dist = new int[20001];
@@ -47,9 +47,13 @@ public class Solution {
         while(!priorityQueue.isEmpty()) {
             // 최소거리가 가장 작은 노드 선택
             Pair curv = priorityQueue.poll();
-            List<Pair> adjacentList = graph.get(curv.vertex);
-            if(adjacentList == null)
+            List<Pair> adjacentList = graph[curv.vertex];
+
+            //만약 현재 노드까지의 거리가 이미 저장된 시작~현재노드의 거리보다 크면 비교할 필요X
+            //현재 노드를 지나가서는 최단거리가 될 수 없음
+            if (curv.weight > dist[curv.vertex])
                 continue;
+
             // 현재 노드에 인접한 노드들 확인
             for(Pair nextv : adjacentList) {
                 // 현재 노드를 거쳐가는 비용이 더 적으면 갱신 후 우선순위 큐에 삽입
